96a942f9451a3256b40eebd1fe317a6d5be96ad2
Frederickbeaud <fredbeaud.fb@gmail.com>
2022-09-22 12:25:35 +0100

    nouvelle modification

--- src/app/retour-livre/retour-livre.component.ts
+++ src/app/retour-livre/retour-livre.component.ts
@@ -1,4 +1,6 @@
+import { getLocaleDateFormat } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-retour-livre',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./retour-livre.component.css']
 })
 export class RetourLivreComponent implements OnInit {
+  
 
-  constructor() { }
+  id: number | undefined;
+  
+  livres: any;
+  liv=this.livres[0];
+  
+  constructor(private route:ActivatedRoute) { }
 
-  ngOnInit(): void {
+  ngOnInit(): {this.loadlivre();
   }
+  loadlivre(){
+    this.id=this.route.snapshot.params['id'];
+    this.id=Number(this.id)-1;
+    this.liv=this.livres[this.id];
+  }
+
+  getColor(){
+    if(this.liv.commander=='oui') return'green';
+        else return 'red';  }
+  onObtenir(){ this.liv.commander='oui';}
+  reinitialiser(){this.liv.commander='non'}
 
 }
